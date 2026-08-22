bc609aa31204bee99741782885a2526f1c5055d1
Abdul Wahab <malikwahab565@gmail.com>
2026-08-22 21:07:26 +0500

    Update footer.jsx

--- components/footer.jsx
+++ components/footer.jsx
@@ -75,7 +75,7 @@ export function Footer() {
           <Link href="/" className="flex items-center gap-4">
 
  <Image
-  src="/images/logo1.png"
+  src="/images/logo.png"
   width={55}
   height={55}
   className="h-auto w-auto max-h-20 object-contain"
